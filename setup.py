e2eaea360242ebce673046619baaac9b35ef8b5b
Gastón Bertolani <gaston.bertolani@gmail.com>
2021-12-05 01:05:15 -0300

    [FIX]Version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires=[
 
 setup(
     name='ai-cv-utils',
-    version='0.0.4',
+    version='0.0.5',
     author='Gaston Alberto Bertolani',
     author_email='gbertolani.in@gmail.com',
     description='AI Computer Vision Utilities',
